99c3f468f329cc2ee9eebbfbe22db24a30b2431b
Bosco Chan <bosco.chw@gmail.com>
2026-01-08 11:13:24 -0800

    style change

--- lib/resistance_web/components/top_bar/top_bar.ex
+++ lib/resistance_web/components/top_bar/top_bar.ex
@@ -1,9 +1,9 @@
 defmodule ResistanceWeb.TopBar do
   use Phoenix.Component
-
+  import ResistanceWeb.CoreComponents
+  import ResistanceWeb.CustomModals
   alias ResistanceWeb.TopBar.QuitButton
   alias ResistanceWeb.TopBar.SoundToggle
-  alias ResistanceWeb.TopBar.HowToPlayShortcut
 
   @doc """
   Creates the top bar
@@ -12,16 +12,27 @@ defmodule ResistanceWeb.TopBar do
   attr :muted, :any, required: true, doc: "Whether the music is muted or not"
   attr :music_file, :any, required: true, doc: "The music file to play"
   attr :show_quit, :boolean, default: false
+  attr :room_code, :string, default: nil, doc: "The room code"
+
 
   def top_bar(assigns) do
     ~H"""
-        <div class="avalon-top-bar">
-          <.live_component module={HowToPlayShortcut} id="how-to-play-shortcut-button" />
-          <.live_component module={SoundToggle} muted={@muted} music_file={@music_file} id="sound-toggle" />
-          <%= if @show_quit do %>
-            <QuitButton.quit_button />
-          <% end %>
-        </div>
+    <div class="avalon-top-bar">
+      <.how_to_play_shortcut />
+      <.live_component module={SoundToggle} muted={@muted} music_file={@music_file} id="sound-toggle" />
+      <%= if @show_quit do %>
+        <QuitButton.quit_button />
+      <% end %>
+    </div>
+    """
+  end
+
+  def how_to_play_shortcut(assigns) do
+    ~H"""
+    <button phx-click={show_modal("help_modal")} aria-label="how-to-play" class="cursor-pointer">
+      <Heroicons.question_mark_circle solid class="h-5 w-5 stroke-current" />
+    </button>
+    <.help_modal />
     """
   end
 end
